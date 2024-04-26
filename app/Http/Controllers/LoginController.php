81d6a11efc03bc77c5691a53bc78d6b9b613b1ce
LAPTOP-SDS-2022\Admin <didahstnursaadah@gmail.com>
2024-04-26 11:28:19 +0700

    E-pengaduan buat pengaduan

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Http\Middleware\cekLogin;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function login()
+    public function login(Request $request): RedirectResponse
     {
-        return view('login');
+        $credentials = $request->validate([
+            'username' => ['required', 'username'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        }
+
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+
+        return back()->withErrors([
+            'username' => 'The provided credentials do not match our records.',
+        ])->onlyInput('username');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
-Route::get('/login', [LoginController::class, 'login']);
+Route::get('/Login', [LoginController::class, 'login']);
